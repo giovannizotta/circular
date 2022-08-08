1e3223b1c9263d3517dec72c4a88c707a0179136
Giovanni Zotta <giovannizotta771@gmail.com>
2022-08-08 18:48:54 +0200

    Create circularDir on startup

--- cmd/circular/main.go
+++ cmd/circular/main.go
@@ -18,8 +18,16 @@ var (
 // This is called after the plugin starts up successfully
 func onInit(plugin *glightning.Plugin, options map[string]glightning.Option, config *glightning.Config) {
 
+	circularDir := config.LightningDir + "/" + node.CIRCULAR_DIR
+	// check if dir exists, otherwise create it
+	if _, err := os.Stat(circularDir); os.IsNotExist(err) {
+		if err := os.Mkdir(circularDir, 0755); err != nil {
+			log.Fatalln(err)
+		}
+	}
+
 	// we redirect stderr to a file, so that we can have panic logs logged in a file
-	if err := redirectStderr(config.LightningDir + "/" + node.CIRCULAR_DIR); err != nil {
+	if err := redirectStderr(circularDir); err != nil {
 		log.Fatalln(err)
 	}
 
